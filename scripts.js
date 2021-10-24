2c69ecb35c870c541b1c9d6924826314082e399e
RanielJohnson <raniel.johnson@gmail.com>
2021-10-24 17:05:02 -0500

    Javascript edited

--- scripts.js
+++ scripts.js
@@ -1,1 +1,18 @@
 /* Add your JavaScript to this file */
+window.onload = function() {
+
+    let button = document.querySelector('button');
+    let message = document.querySelector('.message');
+  
+    button.addEventListener("click", function (element) {
+      element.preventDefault();
+  
+      let email = document.querySelector('#email');     
+      
+      if (email.value.length == 0) {  
+          message.innerHTML = 'Please enter a valid email address.';
+      } else {
+          message.innerHTML = 'Thank you! Your email address ' + email.value + ' has been added to the mailing list!';
+      }
+    });
+  };
